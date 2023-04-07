ef266f6ec5ebd6c685fcbe3ae052fb30a799f672
Shubham Handa <shubham.handa@mavenwave.com>
2023-04-08 03:49:28 +0530

    init Modal

--- src/routes/Admin/Admin.js
+++ src/routes/Admin/Admin.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+import Modal from '../../components/UI/Modal';
 const Admin = () => {
+const [shown, setShown] = useState(false);
+
+const hideModal = () =>{
+    setShown(false);
+}
+
+
 return(
-    <h1>Admin</h1>
+    <>
+    <button onClick={()=>setShown(true)}>Users</button>
+        {shown  && <Modal onClose={hideModal}>
+        <h1>USER</h1>
+        </Modal>
+    }</>
 );
 }
 
